aefae89524db46fe195d348f4db86a8aa2c9b7d8
vlad <vladpolugarov293@gmail.com>
2023-06-12 21:27:12 +0300

    Added js index.html

---
+++ hw-2.js
@@ -0,0 +1,94 @@
+// // Задание 1
+// let a = 20;
+// alert (a);
+
+// // задание 2 
+// let iphone = 2007;
+// alert (iphone);
+
+// // задание 3 
+// let name = 'Brendon Eich';
+// alert (name);
+
+// // задание 4 
+// let numberOne = 10;
+// let numberTwo = 2;
+// alert (numberOne+numberTwo);
+// alert (numberOne-numberTwo);
+// alert (numberOne*numberTwo);
+// alert (numberOne/numberTwo);
+
+// // задание 5
+// let number = 2;
+// let degree = 5;
+// let result = number**degree;
+// alert (result);
+
+// // задание 6
+
+// let b = 9;
+// let c = 2;
+// alert (b%c)
+
+// // задание 7
+// let num = 1;
+// num = num + 5;
+// num = num - 3;
+// num = num * 7;
+// num = num / 3;
+// num = num++;
+// num = num--;
+// alert(num);
+
+// задание 8 
+// let age = prompt ('Сколько вам лет?');
+// alert ( age );
+
+// // задание 9
+// let user = {
+//     name: 'Alex',
+//     age: 25,
+//     isAdmin: true
+// }
+
+// задание 9.1
+// let user = {
+// name: "alex",
+// age: 25,
+// isAdmin: false
+// }
+// user['city of residence'] = 'My city';
+// console.log(user)
+// // задание 9.2
+// let user = {
+// name: "Jony",
+// age: 25,
+// isAdmin: false, 
+// cityOfResidence: "My city"
+// }
+// user.age = 27;
+// console.log (user.age);
+
+// // задание 9.3
+// let user = {
+// name: "Jony",
+// age: 25,
+// isAdmin: false, 
+// cityOfResidence: "My city"
+// }
+// delete user["cityOfResidence"];
+// console.log (user);
+
+// // задание 9.4
+// let user = {
+// name: "Jony",
+// age: 25,
+// isAdmin: false, 
+// cityOfResidence: "My city"
+// }
+// let info = prompt("Что вы хотите узнать о пользователе?", );
+// alert( user[info] ); 
+
+ // задание 10
+// let userName = prompt('Введите как вас зовут?');
+// alert('Добро пожаловать на сайт' + userName + '!');
