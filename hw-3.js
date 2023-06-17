c5a603d3e60ad27d6f3b56ad6f59e5c88804efd0
vlad <vladpolugarov293@gmail.com>
2023-06-17 06:09:07 +0300

    end index.html

---
+++ hw-3.js
@@ -0,0 +1,79 @@
+// // задание 1
+// let password = prompt("Введите пароль");
+
+// if (password === "пароль" || password === "Пароль") {
+//   console.log("Пароль введен верно");
+// } else {
+//   console.log("Пароль введен неправильно");
+// }
+
+// // задание 2
+// let c = 1;
+
+// if (c >= 0 && c <= 10) {
+//   console.log("верно");
+// } else {
+//   console.log("Не верно");
+// }
+
+// // задание 3
+// let e = 101;
+// let d = 5;
+
+// if (e >= 100 || d >= 100) {
+//   console.log("верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// // Задание 4
+// let a = "2";
+// let b = "3";
+// alert(Number(a) + Number(b));
+
+// // Задание 5
+
+// let monthNumber = prompt("Число месяца года");
+
+// switch (monthNumber) {
+//   case '1':
+//     console.log('Зима');
+//     break;
+//   case '2':
+//     console.log('Зима');
+//     break;
+//   case '3':
+//     console.log('Весна');
+//     break;
+//   case '4':
+//     console.log('Весна');
+//     break;
+//   case '5':
+//     console.log('Весна');
+//     break;
+//   case '6':
+//     console.log('Лето');
+//     break;
+//   case '7':
+//     console.log('Лето');
+//     break;
+//   case '8':
+//     console.log('Лето');
+//     break;
+//   case '9':
+//     console.log('Осень');
+//     break;
+//   case '10':
+//     console.log('Осень');
+//     break;
+//   case '11':
+//     console.log('Осень');
+//     break;
+//   case '12':
+//     console.log('Зима');
+//     break;
+
+//   default:
+//     console.log('Ввел что то не то');
+//     break;
+// }
