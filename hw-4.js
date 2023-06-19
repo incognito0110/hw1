10f9c9511ef4aceb2450844b1d133eea06897a3d
vlad <vladpolugarov293@gmail.com>
2023-06-19 17:44:39 +0300

    4 lesson

---
+++ hw-4.js
@@ -0,0 +1,43 @@
+// // задание 1
+// let i = 0;
+// while (i <= 1) {
+//     console.log('Привет');
+//     i++;
+// }
+
+// // задание 2
+// let fromTo = 0;
+// while (fromTo <= 25) {
+//     console.log(fromTo);
+//     fromTo++; 
+// }
+
+// Задание 3 
+// let age = 7;
+// while (age <= 25) {
+//     console.log(age);
+//     age++;
+// }
+
+// Задание 4 
+// const obj = {Коля:'200', Вася:'300', Петя:'400'}
+// for (key in obj){
+// console.log(key + ' Зарплата ' + obj[key] + ' долларов')
+// }
+
+// Задание 5
+// var number = 1000;
+// var num = 0;
+// while (number > 50) {
+//     num++;
+//     number /= 2;
+// }
+// console.log(number);
+// console.log(num);
+
+// задание 6 
+
+for (week = 4; week <= 31;) {
+    week += 7;
+    console.log('Сегодня пятница ' + week + ' Необходимо подготовить отчет!' );
+} 
